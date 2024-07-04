245efb4f4581ea863d3fe07afd14128081de8e19
diego.rosazza <diego.rosazza@truextend.com>
2024-07-04 10:11:42 -0400

    Added Unit of Work Logic

---
+++ Data/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using Data.Repository;
+using Data.Repository.interfaces;
+using JsonFlatFileDataStore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DataStore _store;
+        private readonly IClassRepository _classRepository;
+        private readonly IStudentRepository _studentRepository;
+
+        public UnitOfWork(string filePath)
+        {
+            _store = new DataStore(filePath);
+            _classRepository = new ClassRepository(filePath);
+            _studentRepository = new StudentRepository(filePath);
+        }
+
+        public IClassRepository ClassRepository
+        {
+            get { return _classRepository; }
+        }
+        public IStudentRepository StudentRepository
+        {
+            get { return _studentRepository; }
+        }
+    }
+}
