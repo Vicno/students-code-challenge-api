149f16d326f6d43fe5837ec522b75907a8c68b62
diego.rosazza <diego.rosazza@truextend.com>
2024-07-04 15:01:39 -0400

    Expanded the UOW interface

--- Data/IUnitOfWork.cs
+++ Data/IUnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data.Repository.interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace Data
 {
-    internal interface IUnitOfWork
+    public interface IUnitOfWork
     {
+        IClassRepository ClassRepository {  get; }
+        IStudentRepository StudentRepository { get; }
     }
 }
