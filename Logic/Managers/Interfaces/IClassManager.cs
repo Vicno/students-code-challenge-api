d9ffdaa57b4100eb144c5b79ef980618766d51fc
diego.rosazza <diego.rosazza@truextend.com>
2024-07-04 13:27:02 -0400

    Added base mapper and datalogic to the Logic Layer

---
+++ Logic/Managers/Interfaces/IClassManager.cs
@@ -0,0 +1,19 @@
+﻿
+using Logic.Models;
+
+namespace Logic.Managers.Interfaces
+{
+    public interface IClassManager
+    {
+        IEnumerable<ClassDto> GetAll();
+        ClassDto GetById(Guid classCode);
+
+        ClassDto Create(ClassDto newClass);
+
+        ClassDto Update(ClassDto newClass);
+        ClassDto Delete(ClassDto deletedClass);
+        IEnumerable<ClassDto> GetStudentsForClass(Guid classCode);
+        IEnumerable<ClassDto> AddStudentToClass(Guid classCode, Guid studentId);
+        IEnumerable<ClassDto> RemoveStudentFromClass(Guid classCode, Guid studentId);
+    }
+}
---
+++ Logic/Models/ClassDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Models
+{
+    public class ClassDto
+    {
+        public Guid ClassCode { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<Guid> Students { get; set; }
+    }
+}
+}
