6bb49766bbb9210ba4e8442c0bd4dc66b15a7419
diego.rosazza <diego.rosazza@truextend.com>
2024-07-05 17:42:42 -0400

    Please enter the commit message for your changes. Lines starting

--- Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad3478df373b140f1cb7551671bb60bceeaa95d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfadc0dd5b1d2f8644e9b3afcc5a8b4625049164")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
